627fcda0579ba3339d3c44ca32265079a2e37e0a
Nurasyl Aldan <nurassyl.aldan@gmail.com>
2020-11-17 22:09:12 +0500

    Redux initialized.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import logo from './logo.svg';
-import './App.css';
+import {ADD_DISTRICT} from './actions'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.addDistrict('Kazaly')
+  }
+
+  addDistrict(name) {
+    this.props.dispatch({
+      type: ADD_DISTRICT,
+      name,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.props.store.districts.map((d, index) =>
+            <li key={index}>{d.name}</li>
+          )}
+        </ul>
+
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+	return {
+		store: state,
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		dispatch,
+	}
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
---
+++ src/store.js
@@ -0,0 +1,40 @@
+import {createStore} from 'redux';
+
+/**
+ * Actions
+ */
+import { ADD_DISTRICT } from './actions'
+
+/**
+ * Initial state
+ */
+const initialState = {
+  districts: [{
+    name: 'Qyzylorda',
+  }],
+};
+
+/**
+ * Reducer
+ */
+function rootReducer(state = initialState, action) {
+	switch (action.type) {
+		case ADD_DISTRICT:
+      state.districts.unshift({
+        name: action.name,
+      })
+
+      return Object.assign({}, state, {
+        districts: state.districts
+      })
+    default:
+      return state
+	}
+}
+
+/**
+ * Store
+ */
+let store = createStore(rootReducer);
+
+export default store;
