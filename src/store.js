5f966e268c6166f5a3e05dcec63b9f70d1e728b3
Nurasyl Aldan <nurassyl.aldan@gmail.com>
2020-11-17 23:54:47 +0500

    Now we can save state to the localStorage.

--- src/store.js
+++ src/store.js
@@ -15,57 +15,85 @@ import {
 /**
  * Initial state
  */
-const initialState = {
+var initialState = {
   districts: new Array,
 };
+
+if(initialState.districts.length === 0) {
+  let state = window.localStorage.getItem('state')
+
+  if(state) {
+    state = JSON.parse(state)
+
+    // Hide more content
+    for(let d of state.districts) {
+      d.isMoreShown = false
+    }
+
+    initialState = state
+  }
+}
 
 /**
  * Reducer
  */
-function rootReducer(state = initialState, action) { switch (action.type) { case ADD_DISTRICT:
+function rootReducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_DISTRICT:
       state.districts.unshift({
         isMoreShown: false,
         name: action.name,
         cities: [],
       })
 
-      return Object.assign({}, state, {
+      state = Object.assign({}, state, {
         districts: state.districts
       })
+
+      break
 
 		case REMOVE_DISTRICT:
       _.pullAt(state.districts, action.index)
 
-      return Object.assign({}, state, {
+      state = Object.assign({}, state, {
         districts: state.districts
       })
+
+      break
 
 		case TOGGLE_MORE:
       state.districts[action.index].isMoreShown = !state.districts[action.index].isMoreShown
 
-      return Object.assign({}, state, {
+      state = Object.assign({}, state, {
         districts: state.districts
       })
+
+      break
 
 		case ADD_CITY:
       state.districts[action.districtIndex].cities.unshift({
         name: action.name
       })
 
-      return Object.assign({}, state, {
+      state = Object.assign({}, state, {
         districts: state.districts
       })
+
+      break
 
 		case REMOVE_CITY:
       _.pullAt(state.districts[action.districtIndex].cities, action.index)
 
-      return Object.assign({}, state, {
+      state = Object.assign({}, state, {
         districts: state.districts
       })
 
-    default:
-      return state
+      break
 	}
+
+  window.localStorage.setItem('state', JSON.stringify(state))
+
+  return state
 }
 
 /**
